8315b9bd88754d01a27bae3fbefdac2cc962bf27
piousbox <piousbox@gmail.com>
2017-09-23 16:33:15 -0700

    works with footer-collapse

--- app/components/App/Tgm2.jsx
+++ app/components/App/Tgm2.jsx
@@ -7,18 +7,42 @@ import { Grid, Row, Col,
 
 import arrowLeft  from './images/16x16/arrow-left.png'
 import arrowRight from './images/16x16/arrow-right.png'
+import arrowUp    from './images/16x16/arrow-top.png'
+import arrowDown  from './images/16x16/arrow-bottom.png'
+import avatar     from './images/avatars/2.jpg'
 
 import Report2 from '../Reports/Reports2Show'
+
+class Headers extends React.Component {
+  render () {
+    return(<div>
+  <div className="header header-slim">
+    <a href="#">T.G.M</a>
+  </div>
+  <ul className="header" >
+    <li><a href="#">Cities</a></li>
+    <li><a href="#">Tags</a></li>
+    <li><a href="#">News</a></li>
+  </ul>
+  <div className="header-2">
+    <a href="javascript:;">Home</a> &gt; <a href="#">Tags</a> &gt; <a href="#">Dancing</a> &gt; <a href="#">Salsa</a>
+  </div>
+    </div>)
+  }
+}
 
 class Tgm2 extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = { collapseState: 'right',
+                   collapseFooter: 'up',
     };
 
-    this.collapseLeft = this.collapseLeft.bind(this)
+    this.collapseLeft  = this.collapseLeft.bind(this)
     this.collapseRight = this.collapseRight.bind(this)
+    this.collapseUp    = this.collapseUp.bind(this)
+    this.collapseDown  = this.collapseDown.bind(this)
   }
   
   collapseLeft () {
@@ -41,6 +65,22 @@ class Tgm2 extends React.Component {
     }
   }
 
+  collapseUp () {
+    if (this.state.collapseFooter === 'up') {
+      // nothing
+    } else if (this.state.collapseFooter === 'down') {
+      this.setState({ collapseFooter: 'up' })
+    }
+  }
+
+  collapseDown () {
+    if (this.state.collapseFooter === 'down') {
+      // nothing
+    } else if (this.state.collapseFooter === 'up') {
+      this.setState({ collapseFooter: 'down' })
+    }
+  }
+
   render () {
     console.log('+++ tgm2:', this.props, this.state)
 
@@ -51,23 +91,34 @@ class Tgm2 extends React.Component {
       }
     }
     
+    let rightPane = (<div>
+      <Panel>
+        <h5>{"Alberto's"}</h5>
+        <p>{"I guess it's quite popular and also there are a lot of good dancers regularing that place. Would recommend, if there is no problem with me coming in there the next time, which will be when?"}</p>
+      </Panel>
+      <Panel>
+        <h5>Santa Clara Dance Studio</h5>
+        <p>That&apos;s fridays. I've never been there, and will post a review as soon as I go there to check it out.</p>
+      </Panel>
+      <Panel>
+        <h5>Palomar Ballroom</h5>
+        <p>It&apos;s a bit far from where I live, but Santa Cruz is nice (relatively, with sidenotes) and the population that goes to that ballroom is quite nice.</p>
+      </Panel>
+      <Panel>
+        <h5>Agenda Salsa</h5>
+        <p>That&apos;s Wednesdays, which is nice b/c it&apos;s not on Friday. The instruction is so-so, but you can still meet nice people and have fun there.</p>
+      </Panel>
+      <Panel>
+        <h5>The Starlite Ballroom</h5>
+        <p>Sundays, let&apos;s check it out. Terrible calendar infographic. Cannot be worse I think. Impossible to read. But hey, if the ballroom is okay, I&apos;m in.</p>
+      </Panel>
+    </div>)
+
     return(
       <div className="container">
+        <Headers />
         
-        <div className="header header-slim">
-          <a href="#">T.G.M</a>
-        </div>
-        <ul className="header" >
-          <li><a href="#">Cities</a></li>
-          <li><a href="#">Tags</a></li>
-          <li><a href="#">News</a></li>
-        </ul>
-        
-        <div className="header-2">
-          <a href="javascript:;">Home</a> &gt; <a href="#">Tags</a> &gt; <a href="#">Dancing</a> &gt; <a href="#">Salsa</a>
-        </div>
-
-        <div className={ `folder folder-both folder-collapse-${this.state.collapseState}` } >
+        <div className={ `folder folder-both folder-collapse-${this.state.collapseState} footer-${this.state.collapseFooter}` } >
           <div className="folder folder-left folder-half">
             <ul className="nav nav-tabs">
               <li className=""><a href="#web-design-6" data-toggle="tab" aria-expanded="false">Map</a></li>
@@ -82,7 +133,7 @@ class Tgm2 extends React.Component {
               </div>
             </div>
             <div className="folder-ctrl">
-              <a className='btn-left'  onClick={this.collapseLeft}  ><img  src={ arrowLeft }  alt='' /></a>
+              <a className='btn-left'  onClick={this.collapseLeft}  ><img src={ arrowLeft }  alt='' /></a>
               <a className="btn-right" onClick={this.collapseRight} ><img src={ arrowRight } alt='' /></a>
             </div>
           </div>
@@ -95,29 +146,20 @@ class Tgm2 extends React.Component {
             <div className="tab-wrapper">
               <div className="tab-content">
                 <div className="tab-pane active">
-                  { this.state.rightPane }
-                  <Panel>
-                    <h5>{"Alberto's"}</h5>
-                    <p>{"I guess it's quite popular and also there are a lot of good dancers regularing that place. Would recommend, if there is no problem with me coming in there the next time, which will be when?"}</p>
-                  </Panel>
-                  <Panel>
-                    <h5>Santa Clara Dance Studio</h5>
-                    <p>That&apos;s fridays. I've never been there, and will post a review as soon as I go there to check it out.</p>
-                  </Panel>
-                  <Panel>
-                    <h5>Palomar Ballroom</h5>
-                    <p>It&apos;s a bit far from where I live, but Santa Cruz is nice (relatively, with sidenotes) and the population that goes to that ballroom is quite nice.</p>
-                  </Panel>
-                  <Panel>
-                    <h5>Agenda Salsa</h5>
-                    <p>That&apos;s Wednesdays, which is nice b/c it&apos;s not on Friday. The instruction is so-so, but you can still meet nice people and have fun there.</p>
-                  </Panel>
-                  <Panel>
-                    <h5>The Starlite Ballroom</h5>
-                    <p>Sundays, let&apos;s check it out. Terrible calendar infographic. Cannot be worse I think. Impossible to read. But hey, if the ballroom is okay, I&apos;m in.</p>
-                  </Panel>
+                  { rightPane }
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="folder-footer">
+            <div className="folder-footer-ctrl">
+              <a className="btn-up"   onClick={this.collapseUp}   ><img src={ arrowUp }   alt='' /></a>
+              <a className="btn-down" onClick={this.collapseDown} ><img src={ arrowDown } alt='' /></a>
+            </div>            
+            <div className="folder-footer-content">
+              <img className="avatar" src={ avatar } alt='' />
+              <h5>What's up avatar</h5>
+              abba, aba
             </div>
           </div>
         </div>
