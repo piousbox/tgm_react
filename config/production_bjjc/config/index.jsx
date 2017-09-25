a63067eff04216dfd09444d9fdd66ddc5e861dc1
piousbox <piousbox@gmail.com>
2017-09-25 10:47:30 -0700

    built, with stripe and react?

---
+++ config/production_bjjc/config/index.jsx
@@ -0,0 +1,11 @@
+
+let Config = {
+  envName: "bjjc_production",
+  apiUrl: "http://fighters-guild.piousbox.com",
+  domain: "http://tgm2.piousbox.com", // not used
+  fbAppId: "1805195126438868",
+  stripePublicKey: "pk_live_xQX97vdZNe11A3HBVZc1GX6H",
+}
+
+export default Config
+
